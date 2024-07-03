40189d67c920c5e3b5f8df7a2a936d5d4b24dc27
installer <installer@gmail.com>
2024-07-03 11:14:36 -0500

    First commit

---
+++ hosts/void/variables.nix
@@ -0,0 +1,15 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "sandeng1440";
+  gitEmail = "sandeng1440@gmail.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = true;
+
+  # Program Options
+  browser = "brave"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+}
